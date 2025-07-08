1646b69680877da6e8dbff9851dafc1c33cf3f34
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-07-08 20:48:19 -0300

    feat: add admin routes for role and user management
    - Add role management routes for self-administration
    - Include user management routes with role assignment
    - Add session refresh endpoint for role updates

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\RoleManagementController;
+use App\Http\Controllers\SessionController;
 use \App\Helpers\MoodleAuth;
 
 Route::get('/', function () {
@@ -20,3 +22,16 @@ Route::get('/dashboard', function () {
     }
     return view('dashboard');
 })->name('dashboard');
+
+Route::post('/session/refresh-roles', [SessionController::class, 'refreshRoles'])->name('session.refresh.roles');
+
+Route::middleware(['web'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/roles', [RoleManagementController::class, 'index'])->name('roles');
+    Route::post('/roles/assign', [RoleManagementController::class, 'assignRole'])->name('roles.assign');
+    Route::post('/roles/remove', [RoleManagementController::class, 'removeRole'])->name('roles.remove');
+    
+    Route::get('/users', [RoleManagementController::class, 'users'])->name('users');
+    Route::get('/users/{id}/roles', [RoleManagementController::class, 'userRoles'])->name('user.roles');
+    Route::post('/users/roles/assign', [RoleManagementController::class, 'assignRoleToUser'])->name('user.roles.assign');
+    Route::post('/users/roles/remove', [RoleManagementController::class, 'removeRoleFromUser'])->name('user.roles.remove');
+});
