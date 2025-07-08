9bce8825a8308c04614cdf5160489ab67cbf4f73
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-07-08 18:51:20 -0300

    feat: updated the routes. Added the login, logout and dashboad routes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
+use \App\Helpers\MoodleAuth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    $redirect = MoodleAuth::require();
+    if ($redirect) {
+        return $redirect;
+    }
+    return view('dashboard');
+})->name('dashboard');
